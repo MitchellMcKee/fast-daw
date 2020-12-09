43d8f150683b9e3dd1c74912af92872787f8e48c
Mitchell McKee <mckee.m@northeastern.edu>
2020-12-09 01:43:13 -0500

    multi-track playback with offset

--- fast-daw/src/app/daw/daw-track/daw-track.component.ts
+++ fast-daw/src/app/daw/daw-track/daw-track.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AudioContextService } from 'src/services/audio-context-service';
 
 @Component({
@@ -8,11 +8,42 @@ import { AudioContextService } from 'src/services/audio-context-service';
 })
 export class DawTrackComponent implements OnInit {
 
+  @Input() trackNum: number
+
   name = 'Track Name'
-  audioSrc = 'Audio Src Name'
+  trackSelection = ''
+
+  proxyurl = "https://cors-anywhere.herokuapp.com/";
+  url1 = "https://www2.cs.uic.edu/~i101/SoundFiles/ImperialMarch60.wav"; 
+  url2 = "https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav";
 
   constructor(private audioContextService: AudioContextService) { }
 
   ngOnInit(): void {
   }
+
+  selectAudioTrack = () => {
+    if(this.trackSelection === 'imperialMarch') {
+      this.audioContextService.updateAudioTrackSource(this.trackNum, this.proxyurl + this.url1)
+    } else {
+      this.audioContextService.updateAudioTrackSource(this.trackNum, this.proxyurl + this.url2)
+    }
+  }
+
+  increaseAudioTrackOffset = () => {
+    if(this.trackSelection === 'imperialMarch') {
+      this.audioContextService.increaseAudioTrackOffset(this.trackNum, 0.1)
+    } else {
+      this.audioContextService.increaseAudioTrackOffset(this.trackNum, 0.1)
+    }
+  }
+
+  decreaseAudioTrackOffset = () => {
+    if(this.trackSelection === 'imperialMarch') {
+      this.audioContextService.decreaseAudioTrackOffset(this.trackNum, 0.1)
+    } else {
+      this.audioContextService.decreaseAudioTrackOffset(this.trackNum, 0.1)
+    }
+  }
+
 }
--- fast-daw/src/services/audio-context-service.ts
+++ fast-daw/src/services/audio-context-service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { AudioContext } from 'angular-audio-context';
  
 @Injectable({
@@ -6,26 +6,140 @@ import { AudioContext } from 'angular-audio-context';
 })
 export class AudioContextService {
 
-  sources = []
+  tracks = [
+    {
+      'trackNum': 0,
+      'node': this.audioContext.createBufferSource(),
+      'url': 'url-placeholder',
+      'offset': 0,
+      'gain': 0 
+    }
+  ]
  
-  constructor(private audioContext: AudioContext) { }
-
-  playAudio = () => {
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://www2.cs.uic.edu/~i101/SoundFiles/PinkPanther30.wav"; // site that doesn’t send Access-Control-*
-    fetch(proxyurl + url)
-      .then(response => response.arrayBuffer())
-      .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
-      .then(decodedAudio => {
-        const playSound = this.audioContext.createBufferSource()
-        playSound.buffer = decodedAudio
-        playSound.connect(this.audioContext.destination)
-        playSound.start(this.audioContext.currentTime)
-      })
-      .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+  constructor(private audioContext: AudioContext) { 
+    this.stopAudio()
+    this.tracks = []
   }
 
-  stopAudio = () => {
-    this.audioContext.suspend()
+  playAudio = () => this.audioContext.resume()
+
+  stopAudio = () => this.audioContext.suspend()
+
+  updateAudioTrackSource = (trackNum, url) => {
+    var foundTrackNum = false
+    this.tracks.forEach(track => {
+      if(track.trackNum === trackNum) {
+        foundTrackNum = true
+        fetch(url)
+          .then(response => response.arrayBuffer())
+          .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
+          .then(decodedData => {
+            track.node.connect(this.audioContext.destination)
+            track.node.disconnect(this.audioContext.destination)
+
+            const bufferSource = this.audioContext.createBufferSource()
+
+            bufferSource.buffer = decodedData
+            bufferSource.connect(this.audioContext.destination)
+            bufferSource.start(this.audioContext.currentTime)
+
+            track.node = bufferSource
+            track.url = url
+          })
+          .catch(error => console.log(error))
+      }
+    })
+    if(!foundTrackNum) {  
+      this.addTrack(trackNum)
+      this.updateAudioTrackSource(trackNum, url)
+    }
+  }
+
+  private addTrack = (trackNum) => {
+    var newTrack = {
+      'trackNum': trackNum,
+      'node': this.audioContext.createBufferSource(),
+      'url': 'url-placeholder',
+      'offset': 0,
+      'gain': 0 
+    }
+    this.tracks.push(newTrack)
+  }
+
+  increaseAudioTrackOffset = (trackNum, offset) => {
+    var foundTrackNum = false
+    this.tracks.forEach(track => {
+      if(track.trackNum === trackNum) {
+        foundTrackNum = true
+        fetch(track.url)
+          .then(response => response.arrayBuffer())
+          .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
+          .then(decodedData => {
+            track.node.connect(this.audioContext.destination)
+            track.node.disconnect(this.audioContext.destination)
+
+            const bufferSource = this.audioContext.createBufferSource()
+
+            bufferSource.buffer = decodedData
+            bufferSource.connect(this.audioContext.destination)
+            
+            track.offset += offset
+            bufferSource.start(this.audioContext.currentTime + track.offset)
+
+            track.node = bufferSource
+            
+          })
+          .catch(error => console.log(error))
+      }
+    })
+    if(!foundTrackNum) {  
+      this.addTrack(trackNum)
+      this.increaseAudioTrackOffset(trackNum, offset)
+    }
+  }
+
+  decreaseAudioTrackOffset = (trackNum, offset) => {
+    // var foundTrackNum = false
+    // this.tracks.forEach(track => {
+    //   if(track.trackNum === trackNum) {
+    //     foundTrackNum = true
+    //     fetch(track.url)
+    //       .then(response => response.arrayBuffer())
+    //       .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
+    //       .then(decodedData => {
+    //         track.node.connect(this.audioContext.destination)
+    //         track.node.disconnect(this.audioContext.destination)
+
+    //         const bufferSource = this.audioContext.createBufferSource()
+
+    //         bufferSource.buffer = decodedData
+    //         bufferSource.connect(this.audioContext.destination)
+            
+    //         if(this.audioContext.currentTime + (track.offset - offset) < this.audioContext.currentTime) {
+    //           this.tracks.forEach(track => 
+    //             track.trackNum !== trackNum 
+    //               ? this.increaseAudioTrackOffset(track.trackNum, offset) 
+    //               : bufferSource.start(this.audioContext.currentTime + (track.offset - offset))) 
+    //         } else {
+
+    //         }
+
+    //         track.node = bufferSource
+    //       })
+    //       .catch(error => console.log(error))
+    //   }
+    // })
+    // if(!foundTrackNum) {  
+    //   this.addTrack(trackNum)
+    //   this.decreaseAudioTrackOffset(trackNum, offset)
+    // }
+  }
+
+  updateAudioTrackGain = (trackNum, gain) => {
+    this.tracks.forEach(track => {
+      if(track.trackNum === trackNum) {
+        track.gain = gain
+      }
+    })
   }
 }
