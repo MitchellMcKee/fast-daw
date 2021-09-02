462eb5390df888c6d6d3274fcdde957487025b71
Mitchell McKee <mckee.m@northeastern.edu>
2021-09-02 19:46:06 -0400

    Demo Build Sept 2021

--- fast-daw/src/services/file-service.ts
+++ fast-daw/src/services/file-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 
-const localUrl = 'https://localhost:3200/upload'
-const serverUrl = 'https://ec2-18-216-125-59.us-east-2.compute.amazonaws.com/api/upload'
+const localUrl = 'http://localhost:3200/upload'
+const serverUrl = 'http://ec2-18-216-125-59.us-east-2.compute.amazonaws.com/api/upload'
 
 const url = serverUrl
 
